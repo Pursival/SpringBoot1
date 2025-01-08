f686ceea67cd8aa732093891aed0314404c55e45
IvayloDimitrov <ivajdimitrov@tu-sofia.bg>
2025-01-08 03:17:02 +0200

    SpringBoot1

--- src/main/java/org/example/springboot1/SpringBoot1Application.java
+++ src/main/java/org/example/springboot1/SpringBoot1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBoot1Application {
-
+//something to commit
     public static void main(String[] args) {
         SpringApplication.run(SpringBoot1Application.class, args);
     }
